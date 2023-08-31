40b66283608bdfea9a5b694155384d743f9ec413
hev-giovanni <hev.giovanni@gmail.com>
2023-08-31 16:32:45 -0600

    Create component_Test.js
    Ultima prueba

---
+++ src/components/component_Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export default class component_test extends React.Component{
+    render(){
+
+        return(
+            <div>
+                <p>Desarrollo WEB COMPONENTE_Prueba</p>
+            </div>
+        );
+    }
+
+}
